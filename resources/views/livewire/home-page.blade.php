4088639b8fb6dc71529f3463033a70e10be553a0
muris11 <rifqysaputra1102@gmail.com>
2025-03-09 14:40:56 +0700

    Episode 14

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -5,8 +5,8 @@
       <!-- Grid -->
       <div class="grid md:grid-cols-2 gap-4 md:gap-8 xl:gap-20 md:items-center">
         <div>
-          <h1 class="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white">Start your journey with <span class="text-blue-600">DCodeMania</span></h1>
-          <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Purchase wide varities of electronics products like Smartphones, Laptops, Smartwatches, Television and many more.</p>
+          <h1 class="block text-3xl font-bold text-gray-800 sm:text-4xl lg:text-6xl lg:leading-tight dark:text-white">Mulai belanja di <span class="text-blue-600">Munir Jaya Abadi</span></h1>
+          <p class="mt-3 text-lg text-gray-800 dark:text-gray-400">Dapatkan beras berkualitas dan obat pertanian terbaik hanya di Munir Jaya Abadi. Kami menyediakan solusi untuk kebutuhan pertanian Anda dengan harga terjangkau dan pelayanan terbaik.</p>
   
           <!-- Buttons -->
           <div class="mt-7 grid gap-3 w-full sm:inline-flex">
